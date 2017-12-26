8128dc1e300a3d1ca304bd8f8baee65a31f6b5d7
strahlistvan <strahlistvan0@gmail.com>
2017-12-26 22:06:31 +0100

    Result to striped table

--- find-all-words.js
+++ find-all-words.js
@@ -1,12 +1,36 @@
 var dictionary = getDictionary();
+var longProjectName;
+
+function getCheckedProjectName(shortName, longName) {
+	var j = 0;
+	var shortName  = shortName.toLowerCase();
+	var longName   = longName.toLowerCase();
+	var returnText = "";
+	
+	for (i=0; i<longName.length; ++i) {
+		if(longName[i] == shortName[j]) {
+			returnText += longName[i].toUpperCase();
+			++j;
+		}
+		else {
+			returnText += longName[i];
+		}
+	}
+	
+	if (j !== shortName.length)  {
+		alert("Error!");
+		return null;
+	}
+	
+	return returnText;
+}
 
 function findAllWords(baseText, expectedLength) {
 
 	var allWords = [];	
 	var currentWord = "";
 	
-	baseText = baseText.toLowerCase().replace(/\s+/g, "");
-	
+	baseText = baseText.toLowerCase().replace(/\s+/g, "");	
 	
 	if (!dictionary) {
 		console.log("Dictionary not available");
@@ -18,6 +42,12 @@ function findAllWords(baseText, expectedLength) {
 			
 			if (dictionary.indexOf(currentWord) !== -1 && allWords.indexOf(currentWord) === -1) {
 				console.log("Founded: " + currentWord);
+				
+				//redraw?
+			//	document.getElementById("result").display = 'none';
+			//	document.getElementById("result").display = 'block';
+			//	document.getElementById("result").innerHTML += currentWord + "<br />";
+
 				allWords.push(currentWord);
 			}
 			return;			
@@ -45,14 +75,32 @@ function findAllWords(baseText, expectedLength) {
 function printAllNames(baseText, expectedLength) {
 
 	var allNames = [];
-	var resultDiv = document.getElementById("result");
-	resultDiv.innerHTML = "Please wait!";
+	var resultElem = document.getElementById("result");
+	resultElem.innerHTML = "Please wait!";
 	
+	longProjectName = baseText;
 	allNames = findAllWords(baseText, expectedLength);
-	resultDiv.innerHTML = "";
+	
+	if (allNames.length === 0) {
+		resultElem.innerHTML = "No name found for "+longProjectName+" </ br>";
+	}
+	else {
+		resultElem.innerHTML = "";
+	}
+	
+	var resTable = document.createElement("table");
+	resTable.className = "table table-striped";
 	
 	for (var i=0; i<allNames.length; ++i) {
-		resultDiv.innerHTML += allNames[i] + "<br />";
+		var row = resTable.insertRow(-1);
+		
+		var  nameCell = row.insertCell(-1);
+		nameCell.innerHTML = allNames[i];
+		
+		var longNameCell = row.insertCell(-1)
+		longNameCell.innerHTML = getCheckedProjectName(allNames[i], longProjectName);
+				
 	}
+	resultElem.appendChild(resTable);
 	
 }
